a7463e8e327ffe47a88ee2de32864403290176b7
Rio Noredjo <rio@noredjo.com>
2022-10-25 09:12:30 +0100

    Add new user

--- src/main/java/com/rio/todoappspringboot/Controller/AddUserController.java
+++ src/main/java/com/rio/todoappspringboot/Controller/AddUserController.java
@@ -1,14 +1,12 @@
 package com.rio.todoappspringboot.Controller;
 
 
+import com.rio.todoappspringboot.dao.UserRepository;
 import com.rio.todoappspringboot.entity.User;
 import com.rio.todoappspringboot.service.AddUserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import javax.transaction.Transactional;
 
 @RestController
@@ -17,14 +15,23 @@ import javax.transaction.Transactional;
 public class AddUserController {
 
     private AddUserService addUserService;
+    private UserRepository userRepository;
 
-    public AddUserController(AddUserService addUserService){
+
+    public AddUserController(AddUserService addUserService,
+                             UserRepository userRepository){
         this.addUserService = addUserService;
+        this.userRepository = userRepository;
     }
 
     @PostMapping("/add")
     public ResponseEntity<User> addUser(@RequestBody User newUser) {
         User user = addUserService.addUser(newUser);
         return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
+    @RequestMapping("/find-user-by-email/{emailAddress}")
+    public User findUserByEmail( @PathVariable("emailAddress") String emailAddress){
+        return userRepository.findByEmail(emailAddress);
     }
 }
